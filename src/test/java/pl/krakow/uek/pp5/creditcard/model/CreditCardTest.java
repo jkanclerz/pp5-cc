209f15f0cf412860421c890078caf75434bde930
Jakub <jakub.kanclerz@gmail.com>
2019-10-24 11:16:53 +0200

    add tests for assign value

--- src/test/java/pl/krakow/uek/pp5/creditcard/model/CreditCardTest.java
+++ src/test/java/pl/krakow/uek/pp5/creditcard/model/CreditCardTest.java
@@ -2,6 +2,7 @@ package pl.krakow.uek.pp5.creditcard.model;
 
 import org.junit.Assert;
 import org.junit.Test;
+import pl.krakow.uek.pp5.creditcard.model.exceptions.CreditBelowMinimumException;
 
 import java.math.BigDecimal;
 
@@ -12,10 +13,31 @@ public class CreditCardTest {
     @Test
     public void itAllowAssignLimitToCreditCard() {
         //Arrange // Given
-        CreditCard card = new CreditCard("1234-5678");
+        CreditCard creditCard = new CreditCard("1234-5678");
         //Act // When
-        card.assignLimit(BigDecimal.valueOf(NEW_CREDIT_LIMIT));
+        creditCard.assignLimit(BigDecimal.valueOf(NEW_CREDIT_LIMIT));
         //Assert // Then // Expect
-        Assert.assertTrue(card.getLimit().equals(BigDecimal.valueOf(NEW_CREDIT_LIMIT)));
+        Assert.assertTrue(creditCard.getLimit().equals(BigDecimal.valueOf(NEW_CREDIT_LIMIT)));
     }
+
+    @Test
+    public void itVerifyMinimumCreditValue() {
+
+        CreditCard card = new CreditCard("1234-5678");
+
+        try {
+            card.assignLimit(BigDecimal.valueOf(50));
+            Assert.fail("Exception should be thrown");
+        } catch (CreditBelowMinimumException e) {
+            Assert.assertTrue(true);
+        }
+    }
+
+    @Test
+    public void foo() {
+        CreditCard card = new CreditCard("1234-5678");
+        card.assignLimit(BigDecimal.valueOf(100));
+        Assert.assertTrue(card.getLimit().equals(BigDecimal.valueOf(100)));
+    }
+
 }
